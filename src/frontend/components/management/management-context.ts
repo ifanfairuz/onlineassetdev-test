7dd3d26b3482d18f36812d46f77f0afb88e155da
Ifan Fairuz <ifanfairuz@gmail.com>
2025-11-24 11:56:43 +0700

    add query route support

--- src/frontend/components/management/management-context.ts
+++ src/frontend/components/management/management-context.ts
@@ -1,4 +1,5 @@
-import { inject, onMounted, provide, ref, type Ref } from 'vue'
+import { inject, onBeforeUnmount, onMounted, provide, ref, type Ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
 const KEY = Symbol('management-context')
 
@@ -20,48 +21,73 @@ export interface ManagementContextOptions<Model, CreatePayload> {
   createData: (data: CreatePayload) => Promise<Model>
 }
 
+function parseInteger(value: unknown, default_value: number, allows?: number[]) {
+  if (typeof value !== 'number' && typeof value !== 'string') return default_value
+
+  let result: number
+  if (typeof value === 'string') {
+    result = parseInt(value)
+  } else if (!Number.isInteger(value)) {
+    return default_value
+  } else {
+    result = value as number
+  }
+
+  if (isNaN(result)) {
+    return default_value
+  }
+
+  if (!!allows && !allows.includes(result)) {
+    return default_value
+  }
+
+  return result
+}
+
 export function provideManagementContext<Model, CreatePayload>(
   options: ManagementContextOptions<Model, CreatePayload>,
 ) {
+  const router = useRouter()
+  const route = useRoute()
+  const route_path = route.path
+
   const loading = ref(true)
   const datas = ref<Model[]>([]) as Ref<Model[]>
   const meta = ref<Meta>({
-    current_page: 1,
+    current_page: parseInteger(route.query.page, 1),
     total_pages: 1,
-    per_page: 10,
+    per_page: parseInteger(route.query.per_page, 10, [10, 20, 30, 40, 50]),
     total: 0,
     fetched_at: null,
   })
 
   async function getDatas(page?: number, perPage?: number) {
+    try {
+      loading.value = true
+      const { current_page, per_page } = meta.value
+      const res = await options.fetchDatas(page ?? current_page, perPage ?? per_page)
+      datas.value = res.data
+      meta.value = res.meta
+    } finally {
+      loading.value = false
+    }
+  }
+
+  function go(page?: number, perPage?: number) {
     const { current_page, per_page } = meta.value
-    const res = await options.fetchDatas(page ?? current_page, perPage ?? per_page)
-    datas.value = res.data
-    meta.value = res.meta
+    router.replace({ query: { page: page ?? current_page, per_page: perPage ?? per_page } })
   }
 
   async function next() {
     if (loading.value) return
     if (meta.value.current_page === meta.value.total_pages) return
-
-    try {
-      loading.value = true
-      await getDatas(meta.value.current_page + 1)
-    } finally {
-      loading.value = false
-    }
+    go(meta.value.current_page + 1)
   }
 
   async function prev() {
     if (loading.value) return
     if (meta.value.current_page === 1) return
-
-    try {
-      loading.value = true
-      await getDatas(meta.value.current_page - 1)
-    } finally {
-      loading.value = false
-    }
+    go(meta.value.current_page - 1)
   }
 
   async function add(payload: CreatePayload) {
@@ -78,28 +104,16 @@ export function provideManagementContext<Model, CreatePayload>(
 
   async function setPerPage(perPage: number) {
     if (loading.value) return
-
-    try {
-      meta.value = {
-        ...meta.value,
-        per_page: perPage,
-      }
-      loading.value = true
-      await getDatas(1, perPage)
-    } finally {
-      loading.value = false
+    meta.value = {
+      ...meta.value,
+      per_page: perPage,
     }
+    go(1, perPage)
   }
 
   async function refresh() {
     if (loading.value) return
-
-    try {
-      loading.value = true
-      await getDatas()
-    } finally {
-      loading.value = false
-    }
+    await getDatas()
   }
 
   const context = {
@@ -113,10 +127,20 @@ export function provideManagementContext<Model, CreatePayload>(
     refresh,
   }
 
+  const unsubscribe = router.beforeEach((to) => {
+    if (to.path !== route_path) return
+
+    getDatas(
+      parseInteger(to.query.page, 1),
+      parseInteger(to.query.per_page, 10, [10, 20, 30, 40, 50]),
+    )
+  })
   onMounted(() => {
-    getDatas().finally(() => {
-      loading.value = false
-    })
+    getDatas()
+  })
+
+  onBeforeUnmount(() => {
+    unsubscribe()
   })
 
   provide(KEY, context)
