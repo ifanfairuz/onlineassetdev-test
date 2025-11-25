ffeae51e128dce3e56e5b06d8796ac973ec5f28a
Ifan Fairuz <ifanfairuz@gmail.com>
2025-11-25 14:05:24 +0700

    fix dependency conflict zod

--- src/shared/validation/product.schema.ts
+++ src/shared/validation/product.schema.ts
@@ -2,15 +2,15 @@ import z from 'zod'
 
 export const createProductPayloadSchema = z.object({
   name: z
-    .string("Name can't be empty")
+    .string({ message: "Name can't be empty" })
     .min(1, "Name can't be empty")
     .max(255, "Name can't be more than 255 characters"),
   price: z.coerce
-    .number("Price can't be empty")
+    .number({ message: "Price can't be empty" })
     .min(1, "Price can't be empty")
     .max(1_000_000_000, "Price can't be more than 1,000,000,000"),
   category: z
-    .string("Category can't be empty")
+    .string({ message: "Category can't be empty" })
     .min(1, "Category can't be empty")
     .max(255, "Category can't be more than 255 characters"),
 })
--- src/shared/validation/user.schema.ts
+++ src/shared/validation/user.schema.ts
@@ -2,10 +2,11 @@ import z from 'zod'
 
 export const createUserPayloadSchema = z.object({
   name: z
-    .string("Name can't be empty")
+    .string({ message: "Name can't be empty" })
     .min(1, "Name can't be empty")
     .max(255, "Name can't be more than 255 characters"),
   email: z
+    .string({ message: "Email can't be empty" })
     .email('Email is invalid')
     .max(255, "Email can't be more than 255 characters")
     .transform((email) => email.toLowerCase()),
